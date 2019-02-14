d6f58d2587bdd66f5b860ae5a9529f933d6f9da5
ilabstaff <ilabstaff@172.16.116.81>
2019-02-13 19:29:28 -0800

    bot frogs

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,12 +4,15 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+		Robot bot = new Robot();
 		//3. Ask the user what color they would like the robot to draw
 		
 		//5. Use an if/else statement to set the pen color that the user requested
@@ -21,7 +24,14 @@ public class RobotColorChooser {
 		//4. Set the pen width to 10
 		
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		bot.penDown(); 
+		bot.setSpeed(10);
+		bot.move(100);
+		bot.turn(90);
+		bot.move(100);
+		bot.turn(90);
+		bot.move(100);
+		bot.turn(90);
+		bot.move(100); 
 	}
 }
