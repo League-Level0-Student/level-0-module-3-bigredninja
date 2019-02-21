fc61dbb3cf4689e0e8e7721c2ad54b978d70ecd5
ilabstaff <ilabstaff@172.16.116.77>
2019-02-20 19:28:06 -0800

    highlowbotColorchooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -13,25 +13,39 @@ public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
 		Robot bot = new Robot();
-		//3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 1000; i++) {
+			
 		
+		//3. Ask the user what color they would like the robot to draw
+String color = "";//JOptionPane.showInputDialog("what color do you want?Red or blue? ");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if( color.equals("red")) {
+			bot.setPenColor(Color.red);
+		}
+		else if( color.equals("blue")) {
+			bot.setPenColor(Color.blue);
+		}		 
         //6. If the user doesn’t enter anything, choose a random color
-
+		else if( color.equals("")) {
+			bot.setRandomPenColor();
+		}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		bot.setPenWidth(20);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
 		bot.penDown(); 
-		bot.setSpeed(10);
+		bot.setSpeed(1000);
 		bot.move(100);
 		bot.turn(90);
 		bot.move(100);
 		bot.turn(90);
 		bot.move(100);
 		bot.turn(90);
-		bot.move(100); 
+		bot.move(100);
+		bot.turn(82);
+		}
 	}
+	
+	
 }
